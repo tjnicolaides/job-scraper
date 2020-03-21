0b1658900f6181c839c5c99f7347b1c2571be870
TJ Nicolaides <tj.nicolaides@airbnb.com>
2020-03-21 19:50:02 -0400

    converting index.js to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   console.log('Hello world received a request.');
 
-  const target = process.env.TARGET || 'World';
+  const target = process.env.TARGET || 'World!!';
   res.send(`Hello ${target}!`);
 });
 
-const port = process.env.PORT || 8080;
+const port = Number(process.env.PORT) || 8080;
 app.listen(port, () => {
   console.log('Hello world listening on port', port);
 });
