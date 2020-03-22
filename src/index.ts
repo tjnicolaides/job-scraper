58f2a4fca8b8a633b4e38d64b48a6a6b150cee39
TJ Nicolaides <tj.nicolaides@airbnb.com>
2020-03-21 20:42:01 -0400

    quieting eslint warnings for console in this throwaway file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (_req, res) => {
+  // eslint-disable-next-line no-console
   console.log('Hello world received a request.');
 
   const target = process.env.TARGET || 'World!!';
@@ -11,5 +12,6 @@ app.get('/', (_req, res) => {
 
 const port = Number(process.env.PORT) || 8080;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log('Hello world listening on port', port);
 });
