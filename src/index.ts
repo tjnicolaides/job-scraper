55358f0301e7df1db41d5455686f2498de633d81
TJ Nicolaides <tj.nicolaides@airbnb.com>
2020-03-21 21:07:55 -0400

    refactoring index a little to show what a little of what ts can do. shrug

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 
 const app = express();
+const target: string = process.env.TARGET || 'World, from TypeScript!';
+const responseMessage: string = `Hello ${target}!`;
 
 app.get('/', (_req, res) => {
   // eslint-disable-next-line no-console
   console.log('Hello world received a request.');
-
-  const target = process.env.TARGET || 'World!!';
-  res.send(`Hello ${target}!`);
+  res.send(responseMessage);
 });
 
-const port = Number(process.env.PORT) || 8080, ;
+const port: number = Number(process.env.PORT) || 8080;
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('Hello world listening on port', port);
