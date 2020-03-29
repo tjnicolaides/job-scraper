0bd99a1b342e12654e0ca1f6fdc859770010085d
Steven Nicolaides <schmoops01@gmail.com>
2020-03-29 03:40:23 -0400

    Change to prebuild command and remove ncp package.

--- bin/cloneDataDump.js
+++ bin/cloneDataDump.js
@@ -1,22 +1,29 @@
 const clone = require('git-clone');
 const rimraf = require('rimraf');
+const fs = require('fs');
 const path = require('path');
-const { ncp } = require('ncp');
 
-const tmpDestination = `.${path.sep}tmp${path.sep}dataDump${path.sep}`;
-const tmpData = `${tmpDestination}data${path.sep}`;
-const employersDestination = `.${path.sep}src${path.sep}data${path.sep}employers${path.sep}`;
+const tmpDestination = path.resolve(`.${path.sep}tmp${path.sep}dataDump${path.sep}`);
+const tmpData = path.resolve(`${tmpDestination}${path.sep}data${path.sep}`);
+const employersDestination = path.resolve(`.${path.sep}src${path.sep}data${path.sep}employers${path.sep}`);
 
 const cleanupTmpDirectory = () => rimraf(tmpDestination, () => {
   // eslint-disable-next-line no-console
   console.log(`${tmpDestination} has been removed.\n`);
 });
 
-const transferEmployerFiles = () => ncp(`${tmpData}`, `${employersDestination}`, (err) => {
-  if (err) throw err;
+const transferEmployerFiles = () => {
+  fs.readdirSync(tmpData).forEach((file) => {
+    if (path.extname(file) === '.json') {
+      fs.renameSync(`${tmpData}${path.sep}${file}`, `${employersDestination}${path.sep}${file}`, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+
   // eslint-disable-next-line no-console
   console.log(`dataDump employers information moved to ${employersDestination}\n`);
-});
+};
 
 const cloneRepository = () => {
   cleanupTmpDirectory();
@@ -25,6 +32,11 @@ const cloneRepository = () => {
     () => {
       // eslint-disable-next-line no-console
       console.log(`dataDump repo cloned to ${tmpDestination}\n`);
+      fs.accessSync(tmpData, fs.constants.F_OK, (err) => {
+        // eslint-disable-next-line no-console
+        console.log(`${tmpDestination} ${err ? 'does not exist' : 'exists'}`);
+      });
+
       transferEmployerFiles();
       cleanupTmpDirectory();
     });
