1e1fad4fc5719bba65c2277c26dddf4a5c194357
StevenNic <schmoops01@gmail.com>
2020-03-29 01:05:41 -0400

    Automatically move cloned data to ./src/data

--- bin/cloneDataDump.js
+++ bin/cloneDataDump.js
@@ -1,4 +1,38 @@
 const clone = require('git-clone');
+const rimraf = require('rimraf');
+const path = require('path');
+const { ncp } = require('ncp');
 
-// eslint-disable-next-line no-console
-clone('https://github.com/jobjawn/dataDump', './tmp/dataDump/', { shallow: true }, () => console.log('repo is downloaded'));
+const tmpDestination = `.${path.sep}tmp${path.sep}dataDump${path.sep}`;
+const tmpData = `${tmpDestination}data${path.sep}`;
+const employersDestination = `.${path.sep}src${path.sep}data${path.sep}employers${path.sep}`;
+
+const cleanupTmpDirectory = () => rimraf(tmpDestination, () => {
+  // eslint-disable-next-line no-console
+  console.log(`${tmpDestination} has been removed.\n`);
+});
+
+const transferEmployerFiles = () => ncp(`${tmpData}`, `${employersDestination}`, (err) => {
+  if (err) throw err;
+  // eslint-disable-next-line no-console
+  console.log(`dataDump employers information moved to ${employersDestination}\n`);
+});
+
+const cloneRepository = () => {
+  cleanupTmpDirectory();
+  const repository = 'https://github.com/jobjawn/dataDump';
+  return clone(repository, tmpDestination, { shallow: true },
+    () => {
+      // eslint-disable-next-line no-console
+      console.log(`dataDump repo cloned to ${tmpDestination}\n`);
+      transferEmployerFiles();
+      cleanupTmpDirectory();
+    });
+};
+
+
+const init = () => {
+  cloneRepository();
+};
+
+init();
