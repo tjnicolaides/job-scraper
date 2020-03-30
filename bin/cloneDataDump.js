08b3c12db4151685dcaec4489c348cf29cced780
Steven N <stevennic22@users.noreply.github.com>
2020-03-29 22:50:36 -0400

    Feature/clone data dump (#14)
    * Add dataDump cloning through prebuild script. Fixes #9

---
+++ bin/cloneDataDump.js
@@ -0,0 +1,42 @@
+const clone = require('git-clone');
+const rimraf = require('rimraf');
+const fs = require('fs');
+const path = require('path');
+
+const tmpDestination = path.resolve('./tmp/dataDump/');
+const tmpData = path.resolve(`${tmpDestination}/data/`);
+const employersDestination = path.resolve('./src/data/employers/');
+
+/* eslint-disable no-console */
+
+const cleanupTmpDirectory = () => rimraf(tmpDestination, () => {
+  console.log(`${tmpDestination} has been removed.\n`);
+});
+
+const transferEmployerFiles = () => {
+  fs.accessSync(tmpData, fs.constants.F_OK, (err) => console.log(`${tmpDestination} ${err ? 'does not exist' : 'exists'}\n`));
+
+  fs
+    .readdirSync(tmpData)
+    .filter((file) => file.extname !== '.json')
+    .map((file) => fs.renameSync(path.resolve(`${tmpData}/${file}`), path.resolve(`${employersDestination}/${file}`), (err) => {
+      if (err) throw err;
+    }));
+
+  console.log(`dataDump employers information moved to ${employersDestination}\n`);
+};
+
+const cloneRepository = (repository) => clone(repository, tmpDestination, { shallow: true },
+  () => {
+    console.log(`${repository} cloned to ${tmpDestination}\n`);
+
+    transferEmployerFiles();
+    cleanupTmpDirectory();
+  });
+
+const init = () => {
+  const repository = 'https://github.com/jobjawn/dataDump';
+  cloneRepository(repository);
+};
+
+init();
