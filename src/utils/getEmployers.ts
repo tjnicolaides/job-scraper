1961c2719cd9051b90b65033985027ebb54f9b26
TJ Nicolaides <tj.nicolaides@airbnb.com>
2020-04-01 22:03:30 -0400

    some refactoring of getEmployers

--- src/utils/getEmployers.ts
+++ src/utils/getEmployers.ts
@@ -2,19 +2,28 @@ import fs from 'fs';
 import path from 'path';
 
 const employersDestination = path.resolve('./src/data/employers/');
-// eslint-disable-next-line no-useless-escape
-const re = /\,(?!\s*?[\{\[\"\'\w])/g;
 
-export default async () => fs.readdir(employersDestination, (err, files) => {
+const readFileContents = (file: string) => fs.readFile(path.resolve(`${employersDestination}/${file}`), 'utf8', (err, data) => {
   if (err) throw err;
-  files
+  // eslint-disable-next-line no-useless-escape
+  const re = /\,(?!\s*?[\{\[\"\'\w])/g;
+  const parsedJobInfo = JSON.parse(data.replace(re, ''));
+
+  // eslint-disable-next-line no-console
+  console.log(`${parsedJobInfo.company.name}\n${parsedJobInfo.company.jobs_url}\n`);
+});
+
+const readEmployerFiles = (err: Error | null, files: string[]) => {
+  if (err) throw err;
+  return files
     .filter((file) => path.extname(file) === '.json')
     // eslint-disable-next-line no-shadow
-    .map((file) => fs.readFile(path.resolve(`${employersDestination}/${file}`), 'utf8', (err, data) => {
-      if (err) throw err;
-      const parsedJobInfo = JSON.parse(data.replace(re, ''));
+    .map((file) => readFileContents(file));
+};
 
-      // eslint-disable-next-line no-console
-      console.log(`${parsedJobInfo.company.name}\n${parsedJobInfo.company.jobs_url}\n`);
-    }));
-});
+const getEmployerFiles = () => {
+  fs.readdir(employersDestination, (err, files) => readEmployerFiles(err, files));
+};
+
+
+export default () => getEmployerFiles();
