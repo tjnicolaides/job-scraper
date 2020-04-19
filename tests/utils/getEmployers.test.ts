62a70f7ab5c5d233e27f98e3158180f5c429675e
StevenNic <schmoops01@gmail.com>
2020-04-18 21:19:43 -0400

    Placeholders for new tests.

--- tests/utils/getEmployers.test.ts
+++ tests/utils/getEmployers.test.ts
@@ -11,8 +11,21 @@ describe('cleanEmployerJSON', () => {
 
 describe('getEmployerDetails', () => {
   it('gets relevant Employer Details from JSON files', () => {
-    const invalidJSON = '\{\"company\": \{\"name\":\"Company\"\,\"url\": \"https://example.com\"\,\"location\": \"Old City\"\,\"jobs_url\": \"https://example.com/jobs\"\}\,\"jobs\": \{"updated": "2/11/2019"\,\"listings\": \[\{\"title\": \"Director of Engineering\"\,\"url\": \"https://example.com/jobs/engineering-manager\"\,\"discipline\": \"development\"\,\"level\": \"Management\"\,\"found\": \"2/11/2019\"\}\,\]\}\}';
-    const validJSON = {"name":"Company","url": "https://example.com","jobsUrl": "https://example.com/jobs"};
-    expect(getEmployerDetails(invalidJSON)).toEqual(validJSON);
+    const fullEmployerJSON = '\{\"company\": \{\"name\":\"Company\"\,\"url\": \"https://example.com\"\,\"location\": \"Old City\"\,\"jobs_url\": \"https://example.com/jobs\"\}\,\"jobs\": \{"updated": "2/11/2019"\,\"listings\": \[\{\"title\": \"Director of Engineering\"\,\"url\": \"https://example.com/jobs/engineering-manager\"\,\"discipline\": \"development\"\,\"level\": \"Management\"\,\"found\": \"2/11/2019\"\}\,\]\}\}';
+    const filterEmployer = {"name":"Company","url": "https://example.com","jobsUrl": "https://example.com/jobs"};
+    expect(getEmployerDetails(fullEmployerJSON)).toEqual(filterEmployer);
+  });
+});
+
+
+describe('readFileContents', () => {
+  it.skip('reads contents of Employer Files', () => {
+
+  });
+});
+
+describe('readEmployerFiles', () => {
+  it.skip('iterates over a list of JSON files', () => {
+
   });
 });
