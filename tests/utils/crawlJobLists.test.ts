59d506401e05ecc2144d10b289898676d7c3072e
TJ Nicolaides <tj.nicolaides@airbnb.com>
2020-05-04 09:48:43 -0400

    starting to stub out test with (failing) mock of metascraper

---
+++ tests/utils/crawlJobLists.test.ts
@@ -0,0 +1,36 @@
+import metascraper from 'metascraper';
+import got from 'got';
+import nock from 'nock';
+import crawlJobLists from '../../src/utils/crawlJobList';
+
+jest.genMockFromModule('metascraper').default;
+jest.mock('got');
+
+describe('crawlJobLists', () => {
+  const mockEmployer = {
+    name: 'ABC Company',
+    url: 'https://abc.domain',
+    jobsUrl: 'https://careers.abc.domain',
+  };
+  nock(mockEmployer.url)
+    .get('/')
+    .reply(200, 'Hello world!');
+
+  it('runs metascraper on each employer\'s URL and returns objects with metadata', async () => {
+    const mockResponse = [Promise.resolve({
+      title: 'ABC Company website',
+      author: null,
+      description: 'ABC Comapny is here for you',
+      date: '2020-05-03T21:46:25.000Z',
+      url: 'https://abc.domain',
+    })];
+
+    metascraper.mockReturnValue(mockResponse);
+    const response = await crawlJobLists([mockEmployer]);
+    console.log(response);
+
+    expect(response).toEqual(mockResponse);
+    expect(metascraper).toHaveBeenCalledTimes(1);
+    expect(got).toHaveBeenCalledWith(mockEmployer.url, { rejectUnauthorized: false });
+  });
+});
