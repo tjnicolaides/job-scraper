7474bd2a065a6bd01f26ffff3a04f0ae1e9ad514
StevenNic <schmoops01@gmail.com>
2020-05-31 22:24:20 -0400

    Fixed failing got response.

--- tests/utils/crawlJobLists.test.ts
+++ tests/utils/crawlJobLists.test.ts
@@ -1,12 +1,6 @@
-/* eslint-disable no-console */
-// import metascraper from 'metascraper';
-import got from 'got';
 import nock from 'nock';
 import crawlJobLists from '../../src/utils/crawlJobList';
 import mockEmployerHTML from '../mocks/mockEmployerHTML';
-
-// jest.genMockFromModule('metascraper').default;
-jest.mock('got');
 
 const mockEmployer = {
   name: 'ABC Company',
@@ -20,21 +14,16 @@ describe('crawlJobLists', () => {
     .reply(200, mockEmployerHTML);
 
   it('runs metascraper on each employer\'s URL and returns objects with metadata', async () => {
-    const mockResponse = [Promise.resolve({
+    const mockResponse = [{
       title: 'ABC Company website',
       author: null,
       description: 'ABC Company is here for you',
       date: '2020-05-03T21:46:25.000Z',
       url: 'https://abc.domain',
-    })];
+    }];
 
-    const response = await crawlJobLists([mockEmployer]);
-    console.log(response);
-    console.log(mockResponse);
+    const response = await Promise.all(crawlJobLists([mockEmployer]));
 
-    expect(got).toHaveBeenCalledTimes(1);
-    // expect(response).toEqual(mockResponse);
-    // expect(metascraper).toHaveBeenCalledTimes(1);
-    expect(got).toHaveBeenCalledWith(mockEmployer.url, { rejectUnauthorized: false });
+    expect(response).toEqual(mockResponse);
   });
 });
